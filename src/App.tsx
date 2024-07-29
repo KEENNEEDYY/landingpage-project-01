ef8a95a1be2fb37d286b0efc90d170b5a1beca5a
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-07-28 23:58:15 -0300

    Tailwindcss tests
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as css_app from './css-app';
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className={css_app.main_container}>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img className={css_app.img_css} src={viteLogo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img className={css_app.img_css} src={reactLogo} alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className={css_app.title_css}>Vite + React</h1>
+      <div className={css_app.main_container}>
+        <button className={css_app.button_css} onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
---
+++ src/css-app.ts
@@ -0,0 +1,23 @@
+export const main_container = 
+"flex items-center justify-center " +
+// "bg-lime-500 " +
+// "sm:bg-white " +
+// "md:bg-black " +
+// "lg:bg-orange-500 " +
+"p-6 gap-10 lg:px-8" 
+;
+
+export const img_css = 
+    "h-auto " +
+    "sm:w-20 " +
+    "md:w-30 " +
+    "lg:w-60 "
+;
+
+export const title_css = "bg-white text-black text-5xl p-2 rounded-md";
+
+
+export const button_css = 
+"bg-sky-500 " +
+"border-solid border-2 border-sky-500 p-2 rounded-full"
+// ;
